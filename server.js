380da99ac48d19cf8158a3a83282e4d142ee4030
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-09-05 20:13:18 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+"use strict";
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+const cors = require("cors");
+
+// インスタンス化
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server)
+
+// サーバーのポート番号
+const port = 3000;
+
+// corsの許可、POSTでjsonを受け取るのに必要
+app.use(cors(), express.urlencoded({ extended: true }), express.json());
+
+// socket.io接続時、及びその後の処理
+io.on("connection", (socket) => {
+    console.log("socket.ioに接続しました");
+
+    // 拡張機能のXMLHttpRequestからPOSTメソッドされた時のルーティングの設定
+    app.post("/", (req, res) => {
+        // 受け取ったJSONのdataを表示する
+        console.log(req.body.data);
+        //res.json(req.body);
+
+        // spread message 部分に受け取ったデータを送信する
+        io.emit("spread message", req.body.data);
+    });
+
+    // localhostからの新しいメッセージ受信時の処理 strMessageのstrは文字列の略？
+    socket.on("chat message", (strMessage) => {
+        console.log(`chat message:${strMessage}`);
+
+        // クライアントに受信したメッセージを送信
+        io.emit("spread message", strMessage);
+    })
+
+    // 切断時の処理
+    socket.on("disconnect", () => {
+        console.log("ブラウザが切断されました");
+    });
+});
+
+// GETメソッドされた時のルーティングの設定
+app.get("/", (req, res) => {
+    console.log("/ へのアクセスがありました")
+    /*
+        "/" にアクセスがあった時、res.sendFile()によって、
+        __dirname(このプログラムのディレクトリのパス)/index.htmlを渡す
+    */
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+// サーバーの起動
+server.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
