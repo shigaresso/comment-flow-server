482070f7a6fe7d9c1a213868bd65c54484a5943b
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-10-28 01:36:00 +0900

    コードのフォーマット

--- public/js/index.js
+++ public/js/index.js
@@ -6,24 +6,24 @@ const calcRow = commentMessage => {
         width: strWidth(commentMessage),
         flag: false,
     };
-    comment.speed = (commentMoveWidth + comment.width)/commentDisplayTime;
-    
-    for (let i=0; i<row.length; i++) {
+    comment.speed = (commentMoveWidth + comment.width) / commentDisplayTime;
+
+    for (let i = 0; i < row.length; i++) {
         let relativeSpeed = comment.speed - row[i].speed;
         let timeLag = comment.bornTime - row[i].bornTime;
         let rowCommentRightSide = row[i].speed * timeLag - row[i].width
         let collisionWidth = relativeSpeed * (commentDisplayTime - timeLag) - rowCommentRightSide;
-        
+
         // 行にコメントが存在していない時
         if (timeLag >= commentDisplayTime) {
             createComment(commentMessage, comment, row, i);
             break;
 
-        // コメントが行の右側まで出ていて、衝突しない時
-        } else if (rowCommentRightSide >= 0 && collisionWidth <= 0) {   
+            // コメントが行の右側まで出ていて、衝突しない時
+        } else if (rowCommentRightSide >= 0 && collisionWidth <= 0) {
             createComment(commentMessage, comment, row, i);
             break;
-        
+
         } else if (i == row.length - 1) {
             console.log("コメントを流せませんでした");
         }
@@ -42,26 +42,26 @@ const createComment = (strMessage, comment, row, index) => {
     const placeholder = document.getElementById("placeholder");
     count = count % 100000;
     div_text.id = "text" + count;
-    div_text.setAttribute("data-timelimit", `${comment.bornTime+commentDisplayTime}`)
+    div_text.setAttribute("data-timelimit", `${comment.bornTime + commentDisplayTime}`)
     count++;
     div_text.style.top = commentHeight * comment.flowRow + 'px';
     div_text.appendChild(document.createTextNode(strMessage));
     placeholder.appendChild(div_text);
-            
+
     // GSAP によるアニメーション
-    gsap.to("#"+div_text.id, {
+    gsap.to("#" + div_text.id, {
         duration: 5,
-         x: -1*(document.documentElement.clientWidth+comment.width), 
-         ease: "linear"
-        });
-            
+        x: -1 * (document.documentElement.clientWidth + comment.width),
+        ease: "linear"
+    });
+
 }
 
 const commentDelete = () => {
     let updateTime = Date.now()
     // placeholder要素内の全ての子要素に対して以下の操作を行う
     $(placeholder).children().each((index, i) => {
-        if(updateTime - $(i).attr("data-timelimit") >= 0) $(i).remove();
+        if (updateTime - $(i).attr("data-timelimit") >= 0) $(i).remove();
     });
     console.log("コメント削除関数を呼び出し完了")
 }
--- public/js/worker-dom.js
+++ public/js/worker-dom.js
@@ -1,5 +1,3 @@
-
-
 const calcRow = commentMessage => {
     let comment = {
         bornTime: Date.now(),
@@ -8,24 +6,24 @@ const calcRow = commentMessage => {
         width: measureStringWidth(commentMessage),
         flag: false,
     };
-    comment.speed = (commentMoveWidth + comment.width)/commentDisplayTime;
-    
-    for (let i=0; i<row.length; i++) {
+    comment.speed = (commentMoveWidth + comment.width) / commentDisplayTime;
+
+    for (let i = 0; i < row.length; i++) {
         let relativeSpeed = comment.speed - row[i].speed;
         let timeLag = comment.bornTime - row[i].bornTime;
         let rowCommentRightSide = row[i].speed * timeLag - row[i].width
         let collisionWidth = relativeSpeed * (commentDisplayTime - timeLag) - rowCommentRightSide;
-        
+
         // 行にコメントが存在していない時
         if (timeLag >= commentDisplayTime) {
             createComment(commentMessage, comment, row, i);
             break;
 
-        // コメントが行の右側まで出ていて、衝突しない時
-        } else if (rowCommentRightSide >= 0 && collisionWidth <= 0) {   
+            // コメントが行の右側まで出ていて、衝突しない時
+        } else if (rowCommentRightSide >= 0 && collisionWidth <= 0) {
             createComment(commentMessage, comment, row, i);
             break;
-        
+
         } else if (i == row.length - 1) {
             console.log("コメントを流せませんでした");
         }
@@ -42,11 +40,11 @@ const createComment = (strMessage, comment, row, index) => {
     const div_text = document.createElement("div");
     div_text.setAttribute("class", "chat")
     const placeholder = document.getElementById("placeholder");
-    div_text.setAttribute("data-timelimit", `${comment.bornTime+commentDisplayTime}`)
+    div_text.setAttribute("data-timelimit", `${comment.bornTime + commentDisplayTime}`)
     div_text.style.top = commentHeight * comment.flowRow + 'px';
     div_text.appendChild(document.createTextNode(strMessage));
     placeholder.appendChild(div_text);
-            
+
 }
 
 /**
@@ -54,7 +52,7 @@ const createComment = (strMessage, comment, row, index) => {
  * @param {コメントにする文字列} string 
  * @returns 文字列の幅のピクセル
  */
- const measureStringWidth = (string) => {
+const measureStringWidth = (string) => {
     const span = document.createElement("span");
     const text = document.createTextNode(string);
     span.appendChild(text);
