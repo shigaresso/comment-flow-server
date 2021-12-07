752a7f0bdea9d4bbd0c6ef0382d50a20abf2297f
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-07 17:07:19 +0900

    GSAPも動くようにした

--- public/js/gsap.js
+++ public/js/gsap.js
@@ -1,5 +1,4 @@
 import calcCommentRow from "./modules/calcCommentRow.js";
-import commentDelete from "./modules/commentDelete.js";
 
 const socket = io();
 
@@ -7,30 +6,6 @@ const socket = io();
 socket.on('connect', () => {
     console.log("socket.ioに接続しました");
 });
-
-const createComment = async (strMessage, comment, row, index) => {
-    // 次、この行にコメントが流れる為の条件についての情報
-    row[index].bornTime = comment.bornTime;
-    row[index].speed = comment.speed;
-    row[index].width = comment.width;
-
-    const div_text = document.createElement("div");
-    const placeholder = document.getElementById(`row${index}`);
-    count = count % 100000;
-    div_text.id = "text" + count;
-    div_text.setAttribute("data-timelimit", `${comment.bornTime + commentDisplayTime}`)
-    count++;
-    div_text.appendChild(document.createTextNode(strMessage));
-    placeholder.appendChild(div_text);
-
-    // GSAP によるアニメーション
-    await gsap.to("#" + div_text.id, {
-        duration: 5,
-        x: -1 * (document.documentElement.clientWidth + comment.width + 10),
-        ease: "linear"
-    });
-    div_text.parentNode.removeChild(div_text);
-}
 
 // サーバーからのメッセージ拡散に対する処理
 socket.on('spread message', (strMessage) => {
@@ -43,5 +18,33 @@ socket.on('spread message', (strMessage) => {
     createComment(message, text, row, index);
 });
 
-setInterval(commentDelete, commentDisplayTime);
+
+const createComment = async (strMessage, comment, row, index) => {
+    // 次、この行にコメントが流れる為の条件についての情報
+    row[index].bornTime = comment.bornTime;
+    row[index].speed = comment.speed;
+    row[index].width = comment.width;
+
+    const div_text = document.createElement("div");
+    div_text.innerText = strMessage;
+    count = count % 100000;
+    count++;
+
+    div_text.id = "text" + count;
+    div_text.style.position = 'absolute';
+    div_text.style.paddingLeft = "100%";
+    div_text.style.lineHeight = "1em";
+    const placeholder = document.getElementById(`row${index}`);
+    placeholder.appendChild(div_text);
+
+    // GSAP によるアニメーション
+    await gsap.to("#" + div_text.id, {
+        duration: 5,
+        x: -1 * (document.documentElement.clientWidth + comment.width + 10),
+        ease: "linear",
+        force3D: true,
+    });
+
+    div_text.remove();
+}
 
