256fe8e7f6451637c27e8875470a00e2f6800f14
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-12 17:32:27 +0900

    全てのDOMを計算するのではなく１つ条件に合わなかった時Returnするようにした

--- public/js/modules/commentDelete.js
+++ public/js/modules/commentDelete.js
@@ -2,9 +2,10 @@ const commentDelete = () => {
     const updateTime = Date.now();
     const dom = document.getElementsByClassName("comment");
     for (const iterator of dom) {
-        if (updateTime - iterator.getAttribute("data-timelimit") >= 0) {
-            iterator.remove();
+        if (updateTime - iterator.getAttribute("data-timelimit") < 0) {
+            return;
         }
+        iterator.remove();
     }
 }
 
