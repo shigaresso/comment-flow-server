d654b773c431a18aa4dd36c3e4ed6a91ff758de5
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-16 00:31:53 +0900

    変数宣言を定数でしていい場所を定数に変更

--- public/js/modules/calcCommentRow.js
+++ public/js/modules/calcCommentRow.js
@@ -8,10 +8,10 @@ const calcCommentRow = commentMessage => {
     comment.speed = (commentMoveWidth + comment.width) / commentDisplayTime;
 
     for (let i = 0; i < rows.length; i++) {
-        let relativeSpeed = comment.speed - rows[i].speed;
-        let timeLag = comment.bornTime - rows[i].bornTime;
-        let rowCommentRightSide = rows[i].speed * timeLag - rows[i].width
-        let collisionWidth = relativeSpeed * (commentDisplayTime - timeLag) - rowCommentRightSide;
+        const relativeSpeed = comment.speed - rows[i].speed;
+        const timeLag = comment.bornTime - rows[i].bornTime;
+        const rowCommentRightSide = rows[i].speed * timeLag - rows[i].width
+        const collisionWidth = relativeSpeed * (commentDisplayTime - timeLag) - rowCommentRightSide;
 
         // 行にコメントが存在していないか コメントが行の右側まで出ていて、衝突しない時
         if (timeLag >= commentDisplayTime || rowCommentRightSide >= 0 && collisionWidth <= 0) {
