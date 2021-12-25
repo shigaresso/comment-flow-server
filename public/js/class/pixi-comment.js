405984429b9513c0a1ffea60816e7f2c7509c2ca
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-26 01:48:43 +0900

    インスタンスフィールドをプライベート化した

--- public/js/class/pixi-comment.js
+++ public/js/class/pixi-comment.js
@@ -1,14 +1,16 @@
 class Comment {
+    #pixiInstance
+    #move
 
     constructor(pixiInstance, move) {
-        this.pixiInstance = pixiInstance; // Text のインスタンス
-        this.move = move; // 1 フレームの移動量
+        this.#pixiInstance = pixiInstance; // Text のインスタンス
+        this.#move = move; // 1 フレームの移動量
     }
 
     // フレームの更新処理、画面外にコメントがいった場合、Comment クラスのインスタンスを配列から削除
     render(array) {
-        this.pixiInstance.position.x -= this.move;
-        if (this.pixiInstance.position.x < -this.pixiInstance.width - 1000) {
+        this.#pixiInstance.position.x -= this.#move;
+        if (this.#pixiInstance.position.x < -this.#pixiInstance.width - 1000) {
             array.shift();
             return;
         }
