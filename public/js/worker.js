15087d79122d91bbefdbd435eb4ab0bc092031c2
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-28 04:07:00 +0900

    不要なものを削除

--- public/js/worker.js
+++ public/js/worker.js
@@ -3,7 +3,6 @@ const createComment = (strMessage, bornTime, index) => {
     const div_text = document.createElement("div");
     div_text.className = "comment";
     const placeholder = document.getElementById(`row${index}`);
-    div_text.setAttribute("data-timelimit", `${bornTime + commentDisplayTime}`)
 
     div_text.innerText = strMessage;
     placeholder.appendChild(div_text);
