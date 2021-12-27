f505a3393b152c34a169615f8335eacf0106ef3c
shigaresso <70837100+shigaresso@users.noreply.github.com>
2021-12-28 04:02:45 +0900

    Web-Worker はクラス化が難しいかもしれないので、取り敢えず動作がすることの確認

--- public/js/worker.js
+++ public/js/worker.js
@@ -1,6 +1,25 @@
-import createComment from "./modules/createComment.js";
-import measureStringWidth from "./modules/measure.js";
+const createComment = (strMessage, bornTime, index) => {
 
+    const div_text = document.createElement("div");
+    div_text.className = "comment";
+    const placeholder = document.getElementById(`row${index}`);
+    div_text.setAttribute("data-timelimit", `${bornTime + commentDisplayTime}`)
+
+    div_text.innerText = strMessage;
+    placeholder.appendChild(div_text);
+    div_text.addEventListener("animationend", () => div_text.remove());
+}
+
+const measureStringWidth = (string) => {
+    const span = document.createElement("span");
+    const text = document.createTextNode(string);
+    span.appendChild(text);
+    const ruler = document.getElementById("ruler");
+    ruler.appendChild(span);
+    const stringWidth = ruler.clientWidth;
+    span.remove();
+    return stringWidth;
+}
 const socket = io();
 
 // 接続時の処理
@@ -8,6 +27,7 @@ socket.on('connect', () => {
     console.log("socket.ioに接続しました");
 });
 
+// なぜか Worker スレッドへのパスはこのファイルからではなく、HTML からの相対パスになる
 const myWorker = new Worker("../js/worker-thread.js");
 // サーバーからのメッセージ拡散に対する処理
 socket.on('spread message', (strMessage) => {
@@ -18,26 +38,18 @@ socket.on('spread message', (strMessage) => {
 
     if (window.Worker) {
 
-        // 流れるコメントの作成
-
-        // path の書き方を奇妙にしないと上手くいかない (path: worker.js) だと失敗する
-        // Web-Worker へ送信する
-        // console.log(worker);
+        // Worker スレッドへ送信
         myWorker.postMessage({
             "text": strMessage,
             "commentWidth": commentWidth,
             "commentMoveWidth": commentMoveWidth,
             "commentDisplayTime": commentDisplayTime, "row": rows
         });
-
-
-        // console.log('web-workerに送信済み');
     }
-    // createComment(message, text, row, index);
 });
 
 myWorker.onmessage = e => {
-    createComment(e.data.text, e.data.comment, e.data.row, e.data.index);
-    // console.log(e);
+    createComment(e.data.text, e.data.comment, e.data.index);
+
 }
 
