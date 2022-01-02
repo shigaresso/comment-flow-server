4d7c9f2471e8ad144a9672417227db2f84007771
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-01-02 10:11:55 +0900

    WebWorker をクラス化

---
+++ public/js/class/webWorker.js
@@ -0,0 +1,20 @@
+import { Css } from "./css.js";
+
+class WebWorker extends Css {
+    constructor(commentLane, commentDisplayTime) {
+        super(commentLane, commentDisplayTime);
+        // Worker スレッドへのパスはこのファイルからではなく、HTML からの相対パスになる(そういった仕様)
+        this.myWorker = new Worker("../js/worker-thread.js");
+    }
+
+    createComment(strMessage) {
+        this.myWorker.postMessage({
+            strMessage,
+            width: this.measureStringWidth(strMessage),
+            commentMoveWidth: this.measureStringWidth(strMessage),
+            commentDisplayTime: this.getCommentDisplayTime(),
+        });
+    }
+}
+
+export { WebWorker };
--- public/js/worker-thread.js
+++ public/js/worker-thread.js
@@ -1,44 +1,38 @@
-const rows = Array(11);
-for (let i = 0; i < 11; i++) {
-    rows[i] = {
+const rows = Array(11)
+    .fill()
+    .map(_ => ({
         bornTime: 0,
         speed: 0,
         width: 0,
-    }
-}
+    }));
 
 onmessage = e => {
+    const { strMessage, width, commentMoveWidth, commentDisplayTime } = e.data;
+
     const comment = {
         bornTime: Date.now(),
-        minCollisionWidth: e.data.commentMoveWidth,
-        width: e.data.commentWidth,
-        flag: false,
+        width,
     };
-    comment.speed = (e.data.commentMoveWidth + comment.width) / e.data.commentDisplayTime;
+    comment.speed = (commentMoveWidth + comment.width) / commentDisplayTime;
 
-    // console.log(rows[1].bornTime);
-    for (let i = 0; i < e.data.row.length; i++) {
-        let relativeSpeed = comment.speed - rows[i].speed;
-        let timeLag = comment.bornTime - rows[i].bornTime;
-        let rowCommentRightSide = rows[i].speed * timeLag - rows[i].width
-        let collisionWidth = relativeSpeed * (e.data.commentDisplayTime - timeLag) - rowCommentRightSide;
-
+    for (const [index, row] of rows.entries()) {
+        const relativeSpeed = comment.speed - row.speed;
+        const timeLag = comment.bornTime - row.bornTime;
+        const rowCommentRightSide = row.speed * timeLag - row.width
+        const collisionWidth = relativeSpeed * (commentDisplayTime - timeLag) - rowCommentRightSide;
         // 行にコメントが存在していないか コメントが行の右側まで出ていて、衝突しない時
-        if (timeLag >= e.data.commentDisplayTime || rowCommentRightSide >= 0 && collisionWidth <= 0) {
-            rows[i].bornTime = comment.bornTime;
-            rows[i].speed = comment.speed;
-            rows[i].width = comment.width;
+        if (timeLag >= commentDisplayTime || rowCommentRightSide >= 0 && collisionWidth <= 0) {
+            // 次、この行にコメントが流れる為の条件についての情報
+            rows[index] = comment;
             postMessage({
-                "text": e.data.text,
-                "comment": comment,
-                "row": e.data.row,
-                "index": i
+                strMessage,
+                index,
             });
-            return
+            return;
         }
-        if (i == e.data.row.length - 1) {
-            console.log("コメントを流せませんでした");
-            return false;
-        }
+        // if (index == rows.length - 1) {
+        //     console.log("コメントを流せませんでした");
+        //     return { comment: null };
+        // }
     }
 }
