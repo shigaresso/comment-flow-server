9ca3b0c4853e38cc821f8c6a393865d9ffa924e8
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-03-22 03:29:35 +0900

    様々なファイルに関する設定用変数の移行

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,2 +1,14 @@
-export const row = 11;
+export const rowCount = 5;
+export const rowHeight = Math.round(document.documentElement.clientHeight / rowCount);
 export const commentDisplayTime = 5000;
+export const commentMoveWidth = document.documentElement.clientWidth;
+
+// canvas 用の設定
+export const canvasSetting = {
+    textBaseline: "top",
+    textAlign: "start",
+    lineWidth: "13",
+    font: `900 ${rowHeight}px Segoe UI Emoji`,
+    fillStyle: "white",
+    lineJoin: "round", // 縁取り部分のテキストを尖らないようにする
+}
