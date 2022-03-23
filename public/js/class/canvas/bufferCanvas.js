f1f6fc5ef54df198af673307899b8e7957cb1657
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-03-24 03:41:23 +0900

    この状態だと流れてるコメントが途中で一斉に止まる

---
+++ public/js/class/canvas/bufferCanvas.js
@@ -0,0 +1,57 @@
+export class BufferCanvas {
+    // 表示する canvas のための x 座標
+    #xPosition;
+    // 表示する canvas のための y 座標 (行の index も持って良い)
+    #yPosition;
+    // 配列から削除するかを判定するためのコメント幅
+    #deleteJudgePoint;
+    // 1 フレームの移動量
+    #moveSpeed;
+    // バッファ用の canvas (これを 表示用 canvas に drawImage や putImageData を用いて表示する)
+    #bufferCanvas;
+
+    constructor(textString, xPosition, commentHeight, index, commentWidth, moveSpeed, canvasSetting) {
+        this.#xPosition = xPosition;
+        this.#yPosition = commentHeight * index;
+        this.#deleteJudgePoint = -1 * commentWidth;
+        this.#moveSpeed = moveSpeed;
+        this.#bufferCanvas = this.createBufferCanvas(textString, commentWidth, commentHeight, canvasSetting);
+    }
+
+    // メモリ上に保持するだけで、この canvas 自体は直接画面に表示する事はない
+    createBufferCanvas(textString, commentWidth, commentHeight, canvasSetting) {
+        // バッファ用の canvas の作成
+        const offScreen = document.createElement("canvas");
+        offScreen.width = commentWidth;
+        offScreen.height = commentHeight;
+        const context = offScreen.getContext("2d");
+
+        const {textBaseline, textAlign, lineWidth, font, fillStyle, lineJoin} = canvasSetting;
+        // ここで canvas の幅や高さを指定する必要があるかも (幅はコメント幅、高さはコメント行の高さを用いる)
+        context.textBaseline = textBaseline;
+        context.textAlign = textAlign;
+        context.lineWidth = lineWidth;
+        context.font = font;
+        context.fillStyle = fillStyle;
+        context.lineJoin = lineJoin;
+        context.fillText(textString, 0, 0);
+        return context.getImageData(0, 0, commentWidth, commentHeight);
+    }
+
+    // 表示用 canvas に渡す変数
+    getBufferCanvas() {
+        return { bufferCanvas: this.#bufferCanvas, xPosition: this.#xPosition, yPosition: this.#yPosition }
+    }
+
+    // deletePreviewCanvas() {
+    //     return { xPosition: this.#xPosition + this.#moveSpeed, yPosition: this.#yPosition }
+    // }
+
+    moveComment() {
+        this.#xPosition -= this.#moveSpeed;
+    }
+
+    judgeDelete() {
+        return this.#deleteJudgePoint;
+    }
+}
---
+++ public/js/class/canvas/bufferCanvasState.js
@@ -0,0 +1,31 @@
+import { BufferCanvas } from "./bufferCanvas.js";
+import { CanvasState } from "./canvas.js";
+
+export class BufferCanvasState extends CanvasState {
+    #canvasSetting
+    constructor(commentLane, commentDisplayTime, canvasSetting) {
+        super(commentLane, commentDisplayTime, canvasSetting);
+        this.#canvasSetting = canvasSetting;
+    }
+
+    createComment(commentMessage) {
+        // lineWidth はそのまま渡す
+        const { comment, index } = this.calcCommentRow(this.measureStringWidth(commentMessage));
+        if (!comment) return;
+        const { commentMoveWidth, commentHeight } = this.getWindowSize();
+        const commentMoveSpeed = comment.speed * 1000 / this.getFps();
+        this.commentList.push(new BufferCanvas(commentMessage, commentMoveWidth, commentHeight, index, comment.width, commentMoveSpeed, this.#canvasSetting));
+    }
+
+    drawNextFrame() {
+        // this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // 配列を逆順にループさせないと、ループ中に削除が入るので削除近辺でチラつく
+        for (let i = this.commentList.length-1; i>=0; i--) {
+            this.commentList[i].moveComment();
+            const {bufferCanvas, xPosition, yPosition} = this.commentList[i].getBufferCanvas();
+            this.context.putImageData(bufferCanvas, xPosition, yPosition);
+            if (xPosition < 2*this.commentList[i].judgeDelete()) this.commentList.splice(i);
+        }
+        requestAnimationFrame(() => this.drawNextFrame());
+    }
+}
