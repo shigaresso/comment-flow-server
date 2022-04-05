32ccb048170b5e42fcaeba7661abaf1c40ab7914
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-04-06 00:46:02 +0900

    クライアント側がReactでもコメントを取得できるようにした

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './components/App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App port="10010"/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const App = () => {
-  useState(() => {
-    const socket = io();
+const App = (props: { port: any; }) => {
+  // 最初に呼び出された時のみの処理にしたい
+  useEffect(() => {
+    const socket = io(`http://localhost:${props.port}`);
 
     socket.on("connect", () => {
       console.log("socket.io server に接続しました");
+    });
+
+    socket.on('spread message', (strMessage) => {
+      // OPENRECのコメントがスタンプの場合は処理しない
+      if (strMessage.length == 0) return;
+      console.log(strMessage);
     });
   });
 
